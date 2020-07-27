5e2a988440eb12c097cbce1eb3e805634aca6a5d
Parakoopa <parakoopa@live.de>
2020-07-27 09:24:24 +0200

    Windows build script adjusted for randomizer

--- installer/build-fast.sh
+++ installer/build-fast.sh
@@ -5,7 +5,7 @@ rm build -rf
 rm dist -rf
 
 # Just a build script to build locally without much setup.
-pip uninstall skytemple skytemple-files skytemple-ssb-debugger explorerscript py-desmume skytemple-rust tilequant py-desmume -y
+pip uninstall skytemple skytemple-files skytemple-ssb-debugger explorerscript py-desmume skytemple-rust tilequant py-desmume skytemple-randomizer -y
 pip install python_igraph-*-cp38-cp38-mingw.whl
 pip install py_desmume-*-cp38-cp38-mingw.whl
 pip install skytemple_rust-*-cp38-cp38-mingw.whl
