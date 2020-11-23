bd3cc6620045186c68b22f52f144c28314f3c0e0
Parakoopa <parakoopa@live.de>
2020-11-23 21:08:22 +0100

    0.1.0a15

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def recursive_pkg_files(file_ext):
 
 setup(
     name='skytemple',
-    version='0.1.0a14',
+    version='0.1.0a15',
     packages=find_packages(),
     description='GUI Application to edit the ROM of Pokémon Mystery Dungeon Explorers of Sky (EU/US)',
     long_description=long_description,
@@ -30,13 +30,13 @@ setup(
     url='https://github.com/SkyTemple/skytemple/',
     install_requires=[
         'ndspy >= 3.0.0',
-        'skytemple-files >= 0.1.0a14',
+        'skytemple-files >= 0.1.0a15',
         'skytemple-icons >= 0.1.0',
         'pygobject >= 3.26.0',
         'pycairo >= 1.16.0',
         'natsort >= 7.0.0',
         'tilequant >= 0.4.0',
-        'skytemple-ssb-debugger >= 0.1.0a13',
+        'skytemple-ssb-debugger >= 0.1.0a15',
         'pygal >= 2.4.0',
         'CairoSVG >= 2.4.2'
     ],
