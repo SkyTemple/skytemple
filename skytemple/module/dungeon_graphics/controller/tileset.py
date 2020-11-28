ba75c7417ffd90732c009a1c380f97d9469b68f2
Parakoopa <parakoopa@live.de>
2020-11-28 17:32:25 +0100

    Dungeon tileset: DTEF export/import

--- skytemple/module/dungeon_graphics/controller/tileset.py
+++ skytemple/module/dungeon_graphics/controller/tileset.py
@@ -17,17 +17,24 @@
 
 import itertools
 import math
+import os
 import sys
+import webbrowser
 from collections import OrderedDict
 from functools import partial
 from typing import TYPE_CHECKING, List, Iterable, Callable
 
 import cairo
 
+from skytemple.core.error_handler import display_error
+from skytemple.core.ui_utils import add_dialog_xml_filter
 from skytemple.module.dungeon_graphics.chunk_editor_data_provider.tile_graphics_provider import DungeonTilesProvider
 from skytemple.module.dungeon_graphics.chunk_editor_data_provider.tile_palettes_provider import DungeonPalettesProvider
 from skytemple.module.dungeon_graphics.controller.bg_menu import BgMenuController
 from skytemple.module.tiled_img.dialog_controller.chunk_editor import ChunkEditorController
+from skytemple_dtef.explorers_dtef import ExplorersDtef, VAR0_FN, VAR2_FN, VAR1_FN
+from skytemple_dtef.explorers_dtef_importer import ExplorersDtefImporter
+from skytemple_files.common.xml_util import prettify
 
 try:
     from PIL import Image
@@ -36,15 +43,12 @@ except ImportError:
 from gi.repository import Gtk
 
 from skytemple.controller.main import MainController
-from skytemple.core.error_handler import display_error
 from skytemple.core.img_utils import pil_to_cairo_surface
 from skytemple.core.module_controller import AbstractController
-from skytemple.core.ui_utils import add_dialog_png_filter
 from skytemple.module.dungeon_graphics.dungeon_chunk_drawer import DungeonChunkCellDrawer
-from skytemple.module.tiled_img.dialog_controller.palette_editor import PaletteEditorController
 from skytemple_files.common.util import lcm, chunks
 from skytemple_files.graphics.dma.model import Dma, DmaExtraType, DmaType
-from skytemple_files.graphics.dpc.model import Dpc, DPC_TILING_DIM
+from skytemple_files.graphics.dpc.model import Dpc
 from skytemple_files.graphics.dpci.model import Dpci
 from skytemple_files.graphics.dpl.model import Dpl
 from skytemple_files.graphics.dpla.model import Dpla
@@ -53,7 +57,11 @@ if TYPE_CHECKING:
     from skytemple.module.dungeon_graphics.module import DungeonGraphicsModule
 
 
+URL_HELP = 'https://github.com/SkyTemple/skytemple-dtef/blob/main/docs/SkyTemple.rst'
+
+
 class TilesetController(AbstractController):
+    _last_open_tab_id = 0
     def __init__(self, module: 'DungeonGraphicsModule', item_id: int):
         self.module = module
         self.item_id = item_id
@@ -78,6 +86,7 @@ class TilesetController(AbstractController):
         self.chunks_surfaces: Iterable[Iterable[List[cairo.Surface]]] = []
 
         self._init_chunk_imgs()
+        self.dtef = None
 
         self.menu_controller = BgMenuController(self)
 
@@ -87,8 +96,120 @@ class TilesetController(AbstractController):
         self._init_rule_icon_views()
         self._init_chunk_picker_icon_view()
         self.builder.connect_signals(self)
+        root = self.builder.get_object('editor_root')
+        root.set_current_page(self.__class__._last_open_tab_id)
+        self.on_editor_root_switch_page(None, None, self.__class__._last_open_tab_id)
         self.builder.get_object('label_tileset_name').set_text(f'Dungeon Tileset {self.item_id} Rules')
-        return self.builder.get_object('editor_dungeon_tilesets')
+        self.builder.get_object('label_tileset_name2').set_text(f'Dungeon Tileset {self.item_id}')
+        return root
+
+    def on_editor_root_switch_page(self, w, p, pnum, *args):
+        self.__class__._last_open_tab_id = pnum
+        if pnum == 1:
+            for renderer in self.current_icon_view_renderers:
+                renderer.start()
+        else:
+            self._load_dtef_rendering()
+            for renderer in self.current_icon_view_renderers:
+                renderer.stop()
+
+    # SIMPLE MODE
+
+    def on_btn_import_clicked(self, *args):
+        md = Gtk.MessageDialog(MainController.window(),
+                               Gtk.DialogFlags.DESTROY_WITH_PARENT, Gtk.MessageType.INFO,
+                               Gtk.ButtonsType.OK, "To import select the XML file in the DTEF tileset package. If it "
+                                                   "is still zipped, unzip it first.",
+                               title="SkyTemple")
+        md.run()
+        md.destroy()
+
+        dialog = Gtk.FileChooserDialog(
+            "Import dungeon tileset...",
+            MainController.window(),
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
+        )
+
+        filter = Gtk.FileFilter()
+        filter.set_name("DTEF XML document (*.dtef.xml)")
+        filter.add_pattern("*.dtef.xml")
+        dialog.add_filter(filter)
+        add_dialog_xml_filter(dialog)
+
+        response = dialog.run()
+        fn = dialog.get_filename()
+        dialog.destroy()
+
+        if response == Gtk.ResponseType.OK:
+            try:
+                dirname = os.path.dirname(fn)
+                fn_xml = fn
+                fn_var0 = os.path.join(dirname, VAR0_FN)
+                fn_var1 = os.path.join(dirname, VAR1_FN)
+                fn_var2 = os.path.join(dirname, VAR2_FN)
+                dtef_importer = ExplorersDtefImporter(self.dma, self.dpc, self.dpci, self.dpl, self.dpla)
+                dtef_importer.do_import(
+                    dirname, fn_xml, fn_var0, fn_var1, fn_var2
+                )
+
+                md = Gtk.MessageDialog(MainController.window(),
+                                       Gtk.DialogFlags.DESTROY_WITH_PARENT, Gtk.MessageType.INFO,
+                                       Gtk.ButtonsType.OK, "The tileset was successfully imported.",
+                                       title="Success!")
+                md.run()
+                md.destroy()
+                self.mark_as_modified()
+                MainController.reload_view()
+            except BaseException as e:
+                display_error(
+                    sys.exc_info(),
+                    str(e),
+                    "Error importing the tileset."
+                )
+
+    def on_btn_export_clicked(self, *args):
+        dialog = Gtk.FileChooserDialog(
+            "Export dungeon tileset...",
+            MainController.window(),
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.OK)
+        )
+
+        response = dialog.run()
+        fn = dialog.get_filename()
+        dialog.destroy()
+
+        if response == Gtk.ResponseType.OK:
+            try:
+                # Write XML
+                with open(os.path.join(fn, 'tileset.dtef.xml'), 'w') as f:
+                    f.write(prettify(self.dtef.get_xml()))
+                # Write Tiles
+                var0, var1, var2, rest = self.dtef.get_tiles()
+                var0fn, var1fn, var2fn, restfn = self.dtef.get_filenames()
+                var0.save(os.path.join(fn, var0fn))
+                var1.save(os.path.join(fn, var1fn))
+                var2.save(os.path.join(fn, var2fn))
+                rest.save(os.path.join(fn, restfn))
+
+                md = Gtk.MessageDialog(MainController.window(),
+                                       Gtk.DialogFlags.DESTROY_WITH_PARENT, Gtk.MessageType.INFO,
+                                       Gtk.ButtonsType.OK, "The tileset was successfully exported.",
+                                       title="Success!")
+                md.run()
+                md.destroy()
+            except BaseException as e:
+                display_error(
+                    sys.exc_info(),
+                    str(e),
+                    "Error exporting the tileset."
+                )
+
+    def on_btn_help_clicked(self, *args):
+        webbrowser.open_new_tab(URL_HELP)
+
+    # END SIMPLE MODE
 
     def on_men_chunks_edit_activate(self, *args):
         self.menu_controller.on_men_chunks_layer1_edit_activate()
@@ -279,17 +400,15 @@ class TilesetController(AbstractController):
         else:
             store = icon_view.get_model()
             store.clear()
-            renderer = None
+            self.renderer = None
             for child in icon_view.get_cells():
                 if isinstance(child, DungeonChunkCellDrawer):
-                    renderer = child
+                    self.renderer = child
                     break
 
         init_store(store)
 
         icon_view.select_path(store.get_path(store.get_iter_first()))
-        if renderer is not None:
-            renderer.start()
 
     def _rules_pos_toggle(self, i, state):
         y = i % 3
@@ -344,3 +463,12 @@ class TilesetController(AbstractController):
             )
             self.update_chunks_from_current_rules()
             self.mark_as_modified()
+
+    def _load_dtef_rendering(self):
+        self.dtef = ExplorersDtef(self.dma, self.dpc, self.dpci, self.dpl, self.dpla)
+        box: Gtk.Box = self.builder.get_object('dungeon_image_placeholder')
+        for child in box:
+            box.remove(child)
+        image: Gtk.Image = Gtk.Image.new_from_surface(pil_to_cairo_surface(self.dtef.get_tiles()[0].convert('RGBA')))
+        box.pack_start(image, True, True, 0)
+        box.show_all()
