5085406a653a14ac26cbddad3fc95c5f31897501
Parakoopa <parakoopa@live.de>
2020-12-05 16:36:18 +0100

    Corrected curl commands

--- installer/download-windows-dependencies.sh
+++ installer/download-windows-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl -K https://skytemple.org/build_deps/py_desmume-0.0.3-cp38-cp38-mingw.whl -O
-curl -K https://skytemple.org/build_deps/python_igraph-0.8.2-cp38-cp38-mingw.whl -O
-curl -K https://skytemple.org/build_deps/skytemple_rust-0.0.1-cp38-cp38-mingw.whl -O
-curl -K https://skytemple.org/build_deps/tilequant-0.4.0-cp38-cp38-mingw.whl -O
-curl -K https://skytemple.org/build_deps/armips.exe -O
+curl https://skytemple.org/build_deps/py_desmume-0.0.3-cp38-cp38-mingw.whl -O
+curl https://skytemple.org/build_deps/python_igraph-0.8.2-cp38-cp38-mingw.whl -O
+curl https://skytemple.org/build_deps/skytemple_rust-0.0.1-cp38-cp38-mingw.whl -O
+curl https://skytemple.org/build_deps/tilequant-0.4.0-cp38-cp38-mingw.whl -O
+curl https://skytemple.org/build_deps/armips.exe -O
