a6e5f8c4f8c528ca695f2978c72a0f2773f68ef5
Parakoopa <parakoopa@live.de>
2020-12-07 14:51:38 +0100

    Actions: Don't install binary packages from git

--- installer/install-skytemple-components-from-git.sh
+++ installer/install-skytemple-components-from-git.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ "$(expr substr $(uname -s) 1 5)" != "MINGW" ]; then
-  pip3 install git+https://github.com/SkyTemple/tilequant.git
-  pip3 install git+https://github.com/SkyTemple/skytemple-rust.git
-  pip3 install git+https://github.com/SkyTemple/py-desmume.git
-fi
+#pip3 install git+https://github.com/SkyTemple/tilequant.git
+#pip3 install git+https://github.com/SkyTemple/skytemple-rust.git
+#pip3 install git+https://github.com/SkyTemple/py-desmume.git
 
 pip3 install git+https://github.com/SkyTemple/explorerscript.git
 pip3 install git+https://github.com/SkyTemple/skytemple-files.git
