760a6e90a2f4217ec64c00fea6c7f3bbcc3319ba
Parakoopa <parakoopa@live.de>
2020-12-28 20:37:16 +0100

    Fixes armips for macOS build

--- installer/build-mac.sh
+++ installer/build-mac.sh
@@ -44,7 +44,7 @@ appdir=dist/SkyTemple.app/Contents/MacOS
 sed -i '' 's/run_skytemple/pre_run_skytemple/' dist/SkyTemple.app/Contents/Info.plist
 
 # Create a shell script that sets LD_LIBRARY_PATH and launches SkyTemple
-printf '#!/bin/sh\nLD_LIBRARY_PATH="$(dirname $0)" "$(dirname $0)/run_skytemple"\n' > $appdir/pre_run_skytemple
+printf '#!/bin/sh\nLD_LIBRARY_PATH="$(dirname $0)" PATH="$PATH:$(dirname $0)/skytemple_files/_resources" "$(dirname $0)/run_skytemple"\n' > $appdir/pre_run_skytemple
 chmod +x $appdir/pre_run_skytemple
 
 # Write the version number to files that are read at runtime
