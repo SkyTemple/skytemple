5b907d377ab06adeb04317212784c22ae7300ffe
Parakoopa <parakoopa@live.de>
2021-03-02 10:09:31 +0100

    Bump skytemple-files

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.2.post0'
+__version__ = '1.1.5'
 
 import glob
 import os
@@ -64,7 +64,7 @@ setup(
     url='https://github.com/SkyTemple/skytemple/',
     install_requires=[
         'ndspy >= 3.0.0',
-        'skytemple-files >= 1.1.2.post0',
+        'skytemple-files >= 1.1.5',
         'skytemple-dtef >= 1.1.1',
         'skytemple-icons >= 1.1.0',
         'pygobject >= 3.26.0',
