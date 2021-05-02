4b4199a36e6bd8d54df69c558b6709eda73a3435
Parakoopa <parakoopa@live.de>
2021-05-02 18:10:51 +0200

    Version 1.2.0

--- setup.py
+++ setup.py
@@ -64,14 +64,14 @@ setup(
     url='https://github.com/SkyTemple/skytemple/',
     install_requires=[
         'ndspy >= 3.0.0',
-        'skytemple-files >= 1.2.0',
-        'skytemple-dtef >= 1.1.1',
-        'skytemple-icons >= 1.1.0',
+        'skytemple-files >= 1.2.1',
+        'skytemple-dtef >= 1.1.2',
+        'skytemple-icons >= 1.2.0',
         'pygobject >= 3.26.0',
         'pycairo >= 1.16.0',
         'natsort >= 7.0.0',
         'tilequant >= 0.4.0',
-        'skytemple-ssb-debugger >= 1.1.2',
+        'skytemple-ssb-debugger >= 1.2.0',
         'pygal >= 2.4.0',
         'CairoSVG >= 2.4.2',
         'packaging'
