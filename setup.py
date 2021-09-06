3f0688af26aa4e2fbb39dad9aa78d88677f18f3a
Parakoopa <parakoopa@live.de>
2021-09-06 16:12:27 +0200

    Version 1.3.0rc1 preperation

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.5'
+__version__ = '1.3.0rc1'
 
 import glob
 import os
@@ -64,14 +64,14 @@ setup(
     url='https://github.com/SkyTemple/skytemple/',
     install_requires=[
         'ndspy >= 3.0.0',
-        'skytemple-files >= 1.2.4',
+        'skytemple-files >= 1.3.0rc1',
         'skytemple-dtef >= 1.1.4',
         'skytemple-icons >= 1.2.0',
         'pygobject >= 3.26.0',
         'pycairo >= 1.16.0',
         'natsort >= 7.0.0',
         'tilequant >= 0.4.0',
-        'skytemple-ssb-debugger >= 1.2.5',
+        'skytemple-ssb-debugger >= 1.3.0rc1',
         'pygal >= 2.4.0',
         'CairoSVG >= 2.4.2',
         'packaging'
