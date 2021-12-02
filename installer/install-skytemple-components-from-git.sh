067e42d0371524acbbc35d31a1c9a56f3930101c
Parakoopa <parakoopa@live.de>
2021-12-02 18:49:30 +0100

    Rust Win/Mac: Alternative approach

--- installer/install-skytemple-components-from-git.sh
+++ installer/install-skytemple-components-from-git.sh
@@ -7,4 +7,3 @@ pip3 uninstall -y skytemple-eventserver && pip3 install git+https://github.com/S
 pip3 uninstall -y skytemple-dtef && pip3 install git+https://github.com/SkyTemple/skytemple-dtef.git
 pip3 uninstall -y skytemple-icons && pip3 install git+https://github.com/SkyTemple/skytemple-icons.git
 pip3 uninstall -y skytemple-ssb-debugger && pip3 install git+https://github.com/SkyTemple/skytemple-ssb-debugger.git
-pip3 uninstall -y skytemple-rust && pip3 install git+https://github.com/SkyTemple/skytemple-rust.git
---
+++ installer/install-skytemple-rust.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Installs the latest skytemple-rust version for the current platform.
+# If dev build: Latest is master. Otherwise latest is taken from "release" branch
+# which MUST point to the latest release.
+set -xe
+
+py_version=""
+
+arch="MINGW64"
+if [ "$1" = "i686" ]; then
+  arch="MINGW32"
+fi
+
+branch="release"
+if [ -n "$IS_DEV_BUILD" ]; then
+  branch="master"
+fi
+
+platform="msys2"
+if [ -n "$IS_MACOS" ]; then
+  platform="macos-10.15"
+  arch="x64"
+  py_version="3.9"
+fi
+
+url = "https://nightly.link/SkyTemple/skytemple-rust/workflows/build-test-publish/$branch/wheels-$platform-py$py_version-$arch.zip"
+
+rm -rf tmp_rust || true
+
+mkdir tmp_rust
+cd tmp_rust
+curl -O $url
+pip3 install *.whl
+
+rm -rf tmp_rust || true
