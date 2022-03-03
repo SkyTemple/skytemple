cf7cf918613c2c5ec8f8c356ddc28705a7df2201
Parakoopa <parakoopa@live.de>
2022-03-03 07:09:51 +0100

    do not install SkyTemple components from Git

--- installer/install-skytemple-components-from-git.sh
+++ installer/install-skytemple-components-from-git.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-set -e
-
-pip3 uninstall -y explorerscript && pip3 install git+https://github.com/SkyTemple/explorerscript.git
-pip3 uninstall -y skytemple-files && pip3 install git+https://github.com/SkyTemple/skytemple-files.git
-pip3 uninstall -y skytemple-eventserver && pip3 install git+https://github.com/SkyTemple/skytemple-eventserver.git
-pip3 uninstall -y skytemple-dtef && pip3 install git+https://github.com/SkyTemple/skytemple-dtef.git
-pip3 uninstall -y skytemple-icons && pip3 install git+https://github.com/SkyTemple/skytemple-icons.git
-pip3 uninstall -y skytemple-ssb-debugger && pip3 install git+https://github.com/SkyTemple/skytemple-ssb-debugger.git
+echo "noop"
