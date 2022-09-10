f3543d5b5c336830905a79dd3cbd1f33d9ae7694
Capypara <hello@capypara.de>
2022-09-10 22:17:51 +0200

    Nightly build: Bump MacOS Python version for SkyTemple Rust

--- installer/install-skytemple-rust.sh
+++ installer/install-skytemple-rust.sh
@@ -20,7 +20,7 @@ platform="msys2"
 if [ -n "$IS_MACOS" ]; then
   platform="macos-10.15"
   arch="x64"
-  py_version="3.9"
+  py_version="3.10"
 fi
 
 url="https://nightly.link/SkyTemple/skytemple-rust/workflows/build-test-publish/$branch/wheels-$platform-py$py_version-$arch.zip"
