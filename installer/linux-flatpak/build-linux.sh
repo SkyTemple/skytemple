499bed1278430f57e64ba055979ac120f8453d4a
Capypara <hello@capypara.de>
2022-09-16 21:42:47 +0200

    More tries

--- installer/linux-flatpak/build-linux.sh
+++ installer/linux-flatpak/build-linux.sh
@@ -5,9 +5,7 @@ set -e
 # This configures Flatpak and builds SkyTemple as a Flatpak.
 flatpak install org.gnome.Platform//42 org.gnome.Sdk//42
 
-pip3 install requirements-parser
-python3 ./flatpak-pip-generator.py --requirements-file=../../requirements-mac-windows.txt -o python3-requirements
-
+appstream-util validate-relax org.skytemple.SkyTemple.appdata.xml
 flatpak-builder build-dir --force-clean org.skytemple.SkyTemple.yml
 
 # flatpak-builder --user --install --force-clean build-dir org.skytemple.SkyTemple.yml
