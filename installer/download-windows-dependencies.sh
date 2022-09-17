4b03d1b05a0152c038ec0e00d15124166b572a10
Capypara <hello@capypara.de>
2022-09-17 14:50:00 +0200

    Backports of Actions/Installer changes

--- installer/download-windows-dependencies.sh
+++ installer/download-windows-dependencies.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 if [ "$1" = "i686" ]; then
-  curl https://skytemple.org/build_deps/win32/py_desmume-0.0.4.post2-cp39-cp39-mingw_i686.whl -O
-  curl https://skytemple.org/build_deps/win32/skytemple_rust-1.3.7-cp39-cp39-mingw_i686.whl -O
-  curl https://skytemple.org/build_deps/win32/tilequant-0.4.0.post0-cp39-cp39-mingw_i686.whl -O
-  curl https://skytemple.org/build_deps/win32/igraph-0.8.2-cp39-cp39-mingw_i686.whl -O
+  curl https://skytemple.org/build_deps/win32/py_desmume-0.0.4.post2-cp310-cp310-mingw_i686.whl -O
+  curl https://skytemple.org/build_deps/win32/tilequant-0.4.0.post0-cp310-cp310-mingw_i686.whl -O
+  curl https://skytemple.org/build_deps/win32/igraph-0.8.2-cp310-cp310-mingw_i686.whl -O
+  curl https://skytemple.org/build_deps/win32/skytemple_rust-1.3.7-cp310-cp310-mingw_i686.whl -O
   curl https://skytemple.org/build_deps/armips.exe -O
 else
-  curl https://skytemple.org/build_deps/py_desmume-0.0.4.post2-cp39-cp39-mingw_x86_64.whl -O
-  curl https://skytemple.org/build_deps/skytemple_rust-1.3.7-cp39-cp39-mingw_x86_64.whl -O
-  curl https://skytemple.org/build_deps/tilequant-0.4.0.post0-cp39-cp39-mingw_x86_64.whl -O
-  curl https://skytemple.org/build_deps/igraph-0.8.2-cp39-cp39-mingw_x86_64.whl -O
+  curl https://skytemple.org/build_deps/py_desmume-0.0.4.post2-cp310-cp310-mingw_x86_64.whl -O
+  curl https://skytemple.org/build_deps/tilequant-0.4.0.post0-cp310-cp310-mingw_x86_64.whl -O
+  curl https://skytemple.org/build_deps/igraph-0.8.2-cp310-cp310-mingw_x86_64.whl -O
+  curl https://skytemple.org/build_deps/skytemple_rust-1.3.7-cp310-cp310-mingw_x86_64.whl -O
   curl https://skytemple.org/build_deps/armips.exe -O
 fi
 
