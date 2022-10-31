edb1f2812db35a56ec7ca9d3daef9a0d7b5e46a6
Capypara <hello@capypara.de>
2022-10-31 16:27:39 +0100

    fix typo windows build

--- installer/download-windows-dependencies.sh
+++ installer/download-windows-dependencies.sh
@@ -8,7 +8,7 @@ if [ "$1" = "i686" ]; then
   curl https://skytemple.org/build_deps/armips.exe -O
 else
   curl https://skytemple.org/build_deps/py_desmume-0.0.5.post0-cp310-cp310-mingw_x86_64.whl -O
-  curl https://skytemple.org/build_deps/tilequant-0.4..post01-cp310-cp310-mingw_x86_64.whl -O
+  curl https://skytemple.org/build_deps/tilequant-0.4.1.post0-cp310-cp310-mingw_x86_64.whl -O
   curl https://skytemple.org/build_deps/igraph-0.8.2-cp310-cp310-mingw_x86_64.whl -O
   curl https://skytemple.org/build_deps/skytemple_rust-1.4.0.post0-cp310-cp310-mingw_x86_64.whl -O
   curl https://skytemple.org/build_deps/lru_dict-1.1.8-cp310-cp310-mingw_x86_64.whl -O
