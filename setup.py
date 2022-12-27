c59b388495904e50ef2363168e14611be689dcc9
Capypara <hello@capypara.de>
2022-12-27 10:35:44 +0100

    v1.4.3

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '1.4.2'
+__version__ = '1.4.3'
 
 import glob
 import os
@@ -64,7 +64,7 @@ setup(
     url='https://github.com/SkyTemple/skytemple/',
     install_requires=[
         'ndspy >= 3.0.0',
-        'skytemple-files[spritecollab] >= 1.4.2',
+        'skytemple-files[spritecollab] >= 1.4.3',
         'pmdsky-debug-py',  #  Whatever version skytemple-files requires.
         'skytemple-dtef >= 1.1.4',
         'skytemple-icons >= 1.3.2',
