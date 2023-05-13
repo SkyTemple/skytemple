3239a5fc3b43574209d781ce3d5044dbc17e3f12
Capypara <hello@capypara.de>
2023-05-13 17:20:44 +0200

    Remove legacy scripts, run shell scripts with bash

--- installer/windows_sign.bat
+++
@@ -1,3 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-signtool sign /sha1 f60d5f48096f2af6749bf29da95b1bc07d1272ed /tr http://time.certum.pl /fd sha256 "%~1"
-pause
