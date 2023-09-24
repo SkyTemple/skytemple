f618fe3cd814551449cac538775f2e2382263cb0
Capypara <hello@capypara.de>
2023-09-24 22:58:02 +0200

    Run view loading always on main thread

--- skytemple/controller/main.py
+++ skytemple/controller/main.py
@@ -116,7 +116,7 @@ class MainController:
                 view_cls.__class__,
                 assert_not_none(cls._instance._current_view_item_id),
                 assert_not_none(cls._instance),
-            )
+            ), threadsafe=False
         )
 
     @classmethod
@@ -453,7 +453,7 @@ class MainController:
                 self._current_view_controller_class,
                 self._current_view_item_id,
                 self,
-            )
+            ), threadsafe=False
         )
         # Expand the node
         tree.expand_to_path(path)
