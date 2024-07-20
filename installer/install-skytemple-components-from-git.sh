d89821aeacf215b7fba720db73039c07dd51f287
Capypara <hello@capypara.de>
2024-07-20 16:34:10 +0200

    Don't install ExplorerScript from git anymore

--- installer/install-skytemple-components-from-git.sh
+++ installer/install-skytemple-components-from-git.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-pip3 uninstall -y explorerscript && pip3 install git+https://github.com/SkyTemple/explorerscript.git
 pip3 uninstall -y skytemple-files && pip3 install git+https://github.com/SkyTemple/skytemple-files.git
 pip3 uninstall -y skytemple-eventserver && pip3 install git+https://github.com/SkyTemple/skytemple-eventserver.git
 pip3 uninstall -y skytemple-dtef && pip3 install git+https://github.com/SkyTemple/skytemple-dtef.git
