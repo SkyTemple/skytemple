960973335ce1859e5caa3c43bc4d6c54164d3bb9
Capypara <hello@capypara.de>
2024-07-21 11:58:55 +0200

    more macOS debugging; correct data dirs

--- installer/build-mac.sh
+++ installer/build-mac.sh
@@ -33,6 +33,7 @@ unzip Arc.zip > /dev/null
 curl https://skytemple.org/build_deps/ZorinBlue.zip -O
 unzip ZorinBlue.zip > /dev/null
 mkdir -p share/themes
+python3 -c "from gi.repository import GLib; print(GLib.get_system_data_dirs())"
 export XDG_DATA_DIRS="$(pwd)/share:$XDG_DATA_DIRS"
 cp -a Arc share/themes/Arc
 cp -a Arc-Dark share/themes/Arc-Dark
@@ -40,6 +41,7 @@ cp -a ZorinBlue-Light share/themes/ZorinBlue-Light
 cp -a ZorinBlue-Dark share/themes/ZorinBlue-Dark
 brew install tree
 tree share
+exit 1
 
 # Build the app
 pyinstaller --log-level=DEBUG skytemple-mac.spec --noconfirm
